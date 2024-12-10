2f05e0f0207309e68fcff9f74d0c55ffea619200
uwajacques <uwajacques@gmail.com>
2024-12-10 21:45:10 +0000

    Add auth middleware header validation"

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -3,11 +3,31 @@ package middlewares
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("Running auth middleware")
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 && strings.ToLower(parts[0]) != "bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+
+		// Check the validity of the auth token
+
+		fmt.Printf("Auth token %v\n", tokenString)
 		c.Next()
 	}
 }
